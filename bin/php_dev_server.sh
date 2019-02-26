9554de13a193a327666d4e006888f0f90ba1c368
vimac <vifix.mac@gmail.com>
2019-02-26 22:09:41 +0800

    bug fix

--- bin/php_dev_server.sh
+++ bin/php_dev_server.sh
@@ -3,7 +3,7 @@
 cd `dirname $0`
 
 if [ "$1" = "-d" ]; then
-    php -dxdebug.remote_enable=1 -dxdebug.remote_mode=req -dxdebug.remote_port=9000 -dxdebug.remote_host=127.0.0.1 -S localhost:18888 -t ../public ../public/index.php
+    php -dxdebug.remote_enable=1 -dxdebug.remote_mode=req -dxdebug.remote_port=9000 -dxdebug.remote_host=127.0.0.1 -dxdebug.remote_autostart=1 -S localhost:18888 -t ../public ../public/index.php
 else
     php -S localhost:18888 -t ../public ../public/index.php
 fi
