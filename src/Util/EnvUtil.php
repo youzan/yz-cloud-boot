77ce621c785e9f3744475091487f88fb92bbd84f
xu42 <xu42.cn@gmail.com>
2020-02-10 16:21:01 +0800

    环境变量优先从Apollo读取

--- src/Util/EnvUtil.php
+++ src/Util/EnvUtil.php
@@ -20,12 +20,14 @@ class EnvUtil extends BaseComponent
      */
     public function get(string $varName): ?string
     {
-        $key = str_replace('.', '_', $varName);
-        if (isset($_SERVER[$key])) {
-            return $_SERVER[$key];
+        // 1. 优先从Apollo读取
+        $val = $this->getFromApollo($varName);
+        if (!empty($val)) {
+            return $val;
         }
 
-        return null;
+        // 2. Apollo取不到则从Env取
+        return $this->getFromEnv($varName);
     }
 
     private function getFromApollo(string $varName): ?string
@@ -34,7 +36,11 @@ class EnvUtil extends BaseComponent
             $this->apolloConfig = Yaml::parseFile(Env::APOLLO_FILE);
         }
 
-        return $this->apolloConfig[$varName] ?? null;
+        if (is_array($this->apolloConfig) && isset($this->apolloConfig[$varName])) {
+            return $this->apolloConfig[$varName];
+        }
+
+        return null;
     }
 
     private function getFromEnv(string $varName): ?string
