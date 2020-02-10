a0e8dc3b8a78cef31e09ee4d760b1df29f2c6922
xu42 <xu42.cn@gmail.com>
2020-02-10 15:03:11 +0800

    环境变量优先从Apollo读取 优化重试次数

--- src/Controller/ApolloController.php
+++ src/Controller/ApolloController.php
@@ -7,6 +7,7 @@ use Slim\Http\Response;
 use Symfony\Component\Yaml\Yaml;
 use YouzanCloudBoot\Component\BaseComponent;
 use YouzanCloudBoot\Constant\Env;
+use YouzanCloudBoot\Facades\LogFacade;
 use YouzanCloudBoot\Util\ApolloUtil;
 
 class ApolloController extends BaseComponent
@@ -17,12 +18,30 @@ class ApolloController extends BaseComponent
         /** @var ApolloUtil $apollo */
         $apollo = $this->getContainer()->get('apolloUtil');
 
-        $configSystem = $apollo->get('system');
-        $configApplication = $apollo->get('application');
-        $configAll = array_merge($configApplication, $configSystem);
-
-        file_put_contents(Env::APOLLO_FILE, Yaml::dump($configAll));
+        $this->writeToFile($apollo);
         return $response->withJson(['status' => 'OK']);
+    }
+
+
+    private function writeToFile(ApolloUtil $apollo, $reties = 3)
+    {
+        if ($reties < 0) {
+            LogFacade::warn("Apollo writeToFile. exceeds the maximum retries");
+            return;
+        }
+
+        $configAll = array_merge($apollo->get('system'), $apollo->get('application'));
+        if (empty($configAll)) {
+            LogFacade::warn("Apollo writeToFile. the configAll empty");
+            return $this->writeToFile($apollo, --$reties);
+        }
+
+        // write to file
+        $res = file_put_contents(Env::APOLLO_FILE, Yaml::dump($configAll));
+        if (false === $res) {
+            LogFacade::warn("Apollo writeToFile. write return false");
+            return $this->writeToFile($apollo, --$reties);
+        }
     }
 
 
