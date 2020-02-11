8667ea8a74214d7c70cdb79fb1e91f6c94a61b80
xu42 <xu42.cn@gmail.com>
2020-02-11 21:14:28 +0800

    增加框架预设Apollo任务

--- bin/daemon.php
+++ bin/daemon.php
@@ -12,39 +12,14 @@ $container = (function (): \Psr\Container\ContainerInterface {
     $container['intervalTimerRegistry'] = function (\Psr\Container\ContainerInterface $container) {
         return new \YouzanCloudBoot\Daemon\Registry\IntervalTimerRegistry($container);
     };
+
     \YouzanCloudBoot\Facades\Facade::setFacadeApplication(new \Slim\App($container));
-
-    if (defined('YZCLOUD_BOOT_APP_DIR')) {
-        if (file_exists(YZCLOUD_BOOT_APP_DIR . '/config/intervalTimer.php')) {
-            $intervalTimerRegistry = $container->get("intervalTimerRegistry");
-            (function () use ($intervalTimerRegistry) {
-                require(YZCLOUD_BOOT_APP_DIR . '/config/intervalTimer.php');
-            })();
-        }
-    }
-
     return $container;
 })();
 
 // timing task workers
 $intervalTimerWorker = new \Workerman\Worker();
-$intervalTimerWorker->count = (function (): int {
-
-    if (!defined('YZCLOUD_BOOT_TIMING_WORKER_COUNT')) {
-        return 1;
-    }
-
-    $count = intval(YZCLOUD_BOOT_TIMING_WORKER_COUNT);
-
-    if ($count < 1) {
-        return 1;
-    }
-    if ($count > 10) {
-        return 10;
-    }
-
-    return $count;
-})();
+$intervalTimerWorker->count = 1;
 
 $intervalTimerWorker->onWorkerStart = function ($intervalTimerWorker) use ($container) {
     (new \YouzanCloudBoot\Daemon\Worker\IntervalTimerWorker($container))->onWorkerStart($intervalTimerWorker);
--- src/Daemon/Registry/IntervalTimerRegistry.php
+++ src/Daemon/Registry/IntervalTimerRegistry.php
@@ -2,6 +2,7 @@
 
 namespace YouzanCloudBoot\Daemon\Registry;
 
+use Psr\Container\ContainerInterface;
 use YouzanCloudBoot\Component\BaseComponent;
 
 class IntervalTimerRegistry extends BaseComponent
@@ -9,21 +10,40 @@ class IntervalTimerRegistry extends BaseComponent
 
     private $beanPool = [];
 
-    /**
-     * register
-     * @param string $name
-     * @param callable $callback
-     * @param float $timeInterval
-     */
-    public function register(string $name, callable $callback, float $timeInterval): void
+
+    public function __construct(ContainerInterface $container)
     {
-        $this->beanPool[$name] = ['timeInterval' => $timeInterval, 'callback' => $callback];
+        parent::__construct($container);
+
+        $this->initTask();
     }
 
 
     public function getBeanPool(): array
     {
         return $this->beanPool;
+    }
+
+
+    /**
+     * register
+     * @param string $name
+     * @param callable $callback
+     * @param float $timeInterval
+     */
+    private function register(string $name, callable $callback, float $timeInterval): void
+    {
+        $this->beanPool[$name] = ['timeInterval' => $timeInterval, 'callback' => $callback];
+    }
+
+
+    private function initTask()
+    {
+        $this->register(
+            'yz_cloud_boot_apollo',
+            [new \YouzanCloudBoot\Daemon\Task\DaemonApolloTask($this->getContainer()), 'handle'],
+            60
+        );
     }
 
 }
